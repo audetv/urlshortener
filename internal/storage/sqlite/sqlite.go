b05d53c569d8c99a9993053289d61366f73c6ef9
Aleksey Gusev <audetv@gmail.com>
2024-01-04 23:30:12 +0300

    Fixed sqlite create index query

--- internal/storage/sqlite/sqlite.go
+++ internal/storage/sqlite/sqlite.go
@@ -33,7 +33,7 @@ func New(storagePath string) (*Storage, error) {
 	    alias TEXT NOT NULL UNIQUE,
 	    url TEXT NOT NULL
 	);
-	CREATE INDEX IF NOT EXIST idx_alias ON url(alias);
+	CREATE INDEX IF NOT EXISTS idx_alias ON url(alias);
 `)
 	if err != nil {
 		return nil, fmt.Errorf("%s: %w", op, err)
