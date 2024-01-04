397899bcebb3a8091145545853513146986b4ffd
Aleksey Gusev <audetv@gmail.com>
2024-01-04 12:37:19 +0300

    Added redirect handler

---
+++ internal/http-server/handlers/redirect/redirect.go
@@ -0,0 +1,67 @@
+package redirect
+
+import (
+	"errors"
+	resp "github.com/audetv/urlshortener/internal/lib/api/response"
+	"github.com/audetv/urlshortener/internal/lib/logger/sl"
+	"github.com/audetv/urlshortener/internal/storage"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+	"log/slog"
+	"net/http"
+)
+
+// URLGetter is an interface for getting url by alias.
+//
+//go:generate go run github.com/vektra/mockery/v2@v2.28.2 --name=URLGetter
+type URLGetter interface {
+	GetURL(alias string) (string, error)
+}
+
+func New(log *slog.Logger, urlGetter URLGetter) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "handlers.url.redirect.New"
+
+		log = log.With(
+			slog.String("op", op),
+			slog.String("request_id", middleware.GetReqID(r.Context())),
+		)
+
+		// Роутер Chi позволяет делать так -
+		// получать GET параметры по их именам.
+		// Имена определяются при добавлении хэндлера в роутер, это будет ниже
+		alias := chi.URLParam(r, "alias")
+		if alias == "" {
+			log.Info("alias is empty")
+
+			render.JSON(w, r, resp.Error("not found"))
+
+			return
+		}
+
+		// Находим URL по алиасу в БД
+		resURL, err := urlGetter.GetURL(alias)
+		if errors.Is(err, storage.ErrURLNotFound) {
+			// Не нашли URL сообщаем об этом клиенту
+			log.Info("url not found", "alias", alias)
+
+			render.JSON(w, r, resp.Error("not found"))
+
+			return
+		}
+		if err != nil {
+			// Не удалось осуществить поиск
+			log.Error("failed to get url", sl.Err(err))
+
+			render.JSON(w, r, resp.Error("internal error"))
+
+			return
+		}
+
+		log.Info("got url", slog.String("url", resURL))
+
+		// Делаем редирект на найденный URL
+		http.Redirect(w, r, resURL, http.StatusFound)
+	}
+}
