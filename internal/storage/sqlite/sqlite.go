2429422206da8108eae1eda4a7c35ff3975b58f7
Aleksey Gusev <audetv@gmail.com>
2024-01-05 20:57:46 +0300

    Added sqlite DeleteURL func

--- internal/storage/sqlite/sqlite.go
+++ internal/storage/sqlite/sqlite.go
@@ -96,3 +96,28 @@ func (s *Storage) GetURL(alias string) (string, error) {
 
 	return resURL, nil
 }
+
+func (s *Storage) DeleteURL(alias string) error {
+	const op = "storage.sqlite.DeleteURL"
+
+	stmt, err := s.db.Prepare("DELETE FROM url WHERE alias = ?")
+	if err != nil {
+		return fmt.Errorf("%s: prepare statement: %w", op, err)
+	}
+
+	res, err := stmt.Exec(alias)
+	if err != nil {
+		return fmt.Errorf("%s: execute statement: %w", op, err)
+	}
+
+	// Если ничего не удалили, значит URL не существует
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("%s: failed to get rows affected: %w", op, err)
+	}
+	if rowsAffected == 0 {
+		return storage.ErrURLNotFound
+	}
+
+	return nil
+}
