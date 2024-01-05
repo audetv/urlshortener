e0d8a68e60378e477037a8632937a6c0bf16494c
Aleksey Gusev <audetv@gmail.com>
2024-01-05 13:24:09 +0300

    Added comment to the random package NewRandomString func

--- internal/lib/random/random.go
+++ internal/lib/random/random.go
@@ -5,7 +5,9 @@ import (
 	"time"
 )
 
-// NewRandomString generates random string with given size
+// NewRandomString generates a random string with a given size.
+// It uses a secure random number generator based on the current time.
+// The string can contain uppercase letters, lowercase letters, and digits.
 func NewRandomString(size int) string {
 	rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
 
